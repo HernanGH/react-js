ad2fc1baa817eb8c7bda0e903a43dd46b8223e07
hcondori <hcondori@blockchain.com>
2022-07-06 22:14:35 -0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// obtenemos el div root (la unica linea de vanillaJS permitida)
+const divRoot = document.getElementById('root')
+
+// creamos el virtual dom
+const root = ReactDOM.createRoot(divRoot);
+
+// dibujar los componentes de react aca dentro
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
