2a85e3929578d69cf703596cd5d1c39da842555b
HernanGH <hjcondori04@gmail.com>
2022-07-27 23:21:38 -0300

    nav link done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,40 +2,47 @@ import { Link } from 'react-router-dom';
 import './NavBar.css'
 import CardWidget from "./CardWidget";
 import styled from 'styled-components';
-
-const StyledNavBar = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  ul {
-    display: inline-flex;
-  }
-
-  li {
-    margin-right: 1rem;
-  }
-`
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const NavBar = () => {
   return (
-    <StyledNavBar>
-      Ecommerce
-      <ul>
-        <li>
-          <Link to='/' >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to='/item/1' >
-            Detail
-          </Link>
-        </li>
-      </ul>
-      <CardWidget />
-    </StyledNavBar>
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <LinkContainer to="/">
+            <Nav.Link>
+              Home
+            </Nav.Link>
+          </LinkContainer>
+          <LinkContainer to="/item/1">
+            <Nav.Link>
+              Detail
+            </Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
  
+    // <StyledNavBar>
+    //   Ecommerce
+    //   <ul>
+    //     <li>
+    //       <Link to='/' >
+    //         Home
+    //       </Link>
+    //     </li>
+    //     <li>
+    //       <Link to='/item/1' >
+    //         Detail
+    //       </Link>
+    //     </li>
+    //   </ul>
+    //   <CardWidget />
+    // </StyledNavBar>
 export default NavBar;
